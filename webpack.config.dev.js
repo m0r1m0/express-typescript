7fe716058db6e0e1d421fdc3e87b56af3fc48f8f
pywd <yuki.wd7@gmail.com>
2019-07-06 18:44:02 +0900

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/server.ts',
+  target: 'node',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        options: {
+          configFile: 'tsconfig.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
